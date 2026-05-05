b96a2404f826c5cae6d869bc39403d7c7b51cc84
Reece Cullen <rcc5418@psu.edu>
2026-05-05 16:59:18 -0400

    Testing functionality with perks and addons

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,56 @@
 let killers = [];
+let addons = [];
 
-async function loadKillers() {
-    const res = await fetch("data/killers.json");
-    const data = await res.json();
-    killers = data.killers;
+async function loadData() {
+    const killersRes = await fetch("data/killers.json");
+    const killersJson = await killersRes.json();
+    killers = killersJson.killers;
+
+    const addonsRes = await fetch("data/addons.json");
+    const addonsJson = await addonsRes.json();
+    addons = addonsJson.addons;
+}
+
+function randomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function getRandomKiller() {
-    return killers[Math.floor(Math.random() * killers.length)];
+    return randomItem(killers);
 }
 
-function showRandomKiller() {
-    const killer = getRandomKiller();
+function getRandomPerk(killer) {
+    return randomItem(killer.perks);
+}
 
+function getRandomAddon() {
+    return randomItem(addons);
+}
+
+function generateLoadout() {
+    const killer = getRandomKiller();
+    const perk = getRandomPerk(killer);
+
+    const addon1 = getRandomAddon();
+    const addon2 = getRandomAddon(); // hope you don't roll double Iri LOL
+
+    // Render items
     document.getElementById("killer-name").textContent = killer.name;
     document.getElementById("killer-img").src = killer.image;
+
+    const perkImg = typeof perk === "string" ? perk : perk.image;
+    document.getElementById("perk-img").src = perkImg;
+
+    const addon1Img = typeof addon1 === "string" ? addon1 : addon1.image;
+    const addon2Img = typeof addon2 === "string" ? addon2 : addon2.image;
+
+    document.getElementById("addon1-img").src = addon1Img;
+    document.getElementById("addon2-img").src = addon2Img;
 }
 
 window.onload = async () => {
-    await loadKillers();
-    showRandomKiller();
+    await loadData();
+    generateLoadout();
+
+    document.getElementById("roll-btn").addEventListener("click", generateLoadout);
 };
